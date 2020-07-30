1edc73165b0e356432f1981e3c7f278f74589f29
Erick Iwamoto <erick_zen_@hotmail.com>
2020-07-30 18:36:26 -0300

    fix: ignore dist

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from './app';
 
-app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!');
+const PORT = process.env.PORT || '3333';
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server started on port ${PORT}`);
 });
